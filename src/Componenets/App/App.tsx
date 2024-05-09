b75defc9de57765ce5f3775a0601485ccddd4e49
ComputerEngineer1007 <93746222+baja87@users.noreply.github.com>
2024-05-09 15:26:05 -0400

    654645

--- src/Componenets/App/App.tsx
+++ src/Componenets/App/App.tsx
@@ -89,7 +89,14 @@ class App extends React.Component<any, AppState> {
 
     render(){
         return <div className="App">
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul>
             <h1> PokeDex </h1>
+
             <PokeDex 
                 searchedPokemons={this.state.searchPokemons}
                 selectedPokemon={this.state.selectedPokemon}
